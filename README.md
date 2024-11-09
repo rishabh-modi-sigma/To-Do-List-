f37106caf1be3430ecafb482b8b3afd7a5d276d4
Rishabh Modi <rishabhmodi338@gmail.com>
2024-11-09 21:35:17 +0530

    Add new files

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # To-Do-List-
 this is my first git repository To-Do List.
+
+
+# Owner
+RISHABH MODI
+
+# web sit
+To-Do List
---
+++ app.js
@@ -0,0 +1,44 @@
+let input = document.getElementById("input-box");
+let listcontainer = document.querySelector(".list-container");
+
+function addTask(){
+    try{
+        if(input.value === ''){
+            alert("you must write something");
+        }else{
+        let li = document.createElement("li");
+        li.innerHTML = input.value;
+        listcontainer.appendChild(li);
+        let span= document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+       }
+    }
+    catch (e){
+        console.log("ERROR CATCHED =",e);
+    }
+input.value = "";
+saveData();
+}
+
+listcontainer.addEventListener("click", function (e){
+    try{
+        if(e.target.tagName === "LI"){
+            e.target.classList.toggle("checked");
+            saveData();
+        }else if(e.target.tagName === "SPAN"){
+            e.target.parentElement.remove();
+        }
+        saveData();
+    }catch(e){
+        console.log("ERROR CATCHED=",e);
+    }
+},false);
+
+function saveData(){
+ localStorage.setItem("data",listcontainer.innerHTML); 
+}
+function showTask(){
+  listcontainer.innerHTML = localStorage.getItem("data");
+}
+showTask();
